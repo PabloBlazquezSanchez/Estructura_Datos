9b1cd1d77355102696b7d6540d4a593fe34e0612
RaulJDlCRUZ <114936538+RaulJDlCRUZ@users.noreply.github.com>
2022-10-03 18:32:42 +0200

    pila
    pila

--- practica_s1/src/edLineales2022_23/Pila.java
+++ practica_s1/src/edLineales2022_23/Pila.java
@@ -1,6 +1,6 @@
 package edLineales2022_23;
 
-public interface StackRJC<R> { //Type Generico lo llamo R
+public interface Pila<R> { //Type Generico lo llamo R
 	public void introducir(R element);
 	public R sacar() throws PilaVaciaExcepcion; //no puedes borrar y consultar si la pila esta vacia
 	public R cima() throws PilaVaciaExcepcion; //no puedes consultar ultimo elemento si la pila esta vacia
