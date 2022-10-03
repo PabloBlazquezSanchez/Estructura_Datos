fa89be9bc7d6b1f405fafa962fbe5cdae2e1c575
Jesús <Jesus.Fdez1@alu.uclm.es>
2022-10-03 18:24:11 +0200

    KO

--- practica_s1/src/edLineales2022_23/StackJF.java
+++ practica_s1/src/edLineales2022_23/StackJF.java
@@ -1,5 +1,9 @@
 package edLineales2022_23;
 
-public class StackJF {
-
+public interface StackJF {
+		public void push(T element);
+		public T pop() throws EmptyStackException;
+		public T top() throws EmptyStackException;
+		public boolean isEmpty();
+		public int size();
 }
