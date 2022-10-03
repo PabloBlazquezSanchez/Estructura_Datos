27fa3a86dcfd4e7986ae7ec81fd2f475879f64f7
PabloBlazquezSanchez <Pablo.Blazquez1@alu.uclm.es>
2022-10-03 18:06:28 +0200

    p

--- practica_s1/src/ejemplo/Hello_world.java
+++
@@ -1,9 +0,0 @@
-package ejemplo;
-
-public class Hello_world {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Esto lo estoy poniendo con JDK 17");
-	}
-}
