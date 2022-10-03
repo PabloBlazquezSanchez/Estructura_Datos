34e0d95f27489ee767224c5001d0b04ac0f96085
RaulJDlCRUZ <114936538+RaulJDlCRUZ@users.noreply.github.com>
2022-10-03 17:45:27 +0200

    sin corchete
    quitao

--- practica_s1/src/ejemplo/Hello_world.java
+++ practica_s1/src/ejemplo/Hello_world.java
@@ -4,7 +4,6 @@ public class Hello_world {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello my friend");
+		System.out.println("Hello my friend 2");
 	}
-
 }
