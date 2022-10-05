42a694239d2b0c0d5b89159b22a8b10386f72ffc
RaulJDlCRUZ <114936538+RaulJDlCRUZ@users.noreply.github.com>
2022-10-05 18:00:53 +0200

    keep working
    pila estatica en desarrollo

--- practica_s1/src/edLineales2022_23/Pila.java
+++ practica_s1/src/edLineales2022_23/Pila.java
@@ -1,7 +1,7 @@
 package edLineales2022_23;
 
 public interface Pila<R> { //Type Generico lo llamo R
-	public void introducir(R element);
+	public void introducir(R elemento);
 	public R sacar() throws PilaVaciaExcepcion; //no puedes borrar y consultar si la pila esta vacia
 	public R cima() throws PilaVaciaExcepcion; //no puedes consultar ultimo elemento si la pila esta vacia
 	public boolean estaVacia();
---
+++ practica_s1/src/edLineales2022_23/PilaEstaticaRJC.java
@@ -0,0 +1,35 @@
+package edLineales2022_23;
+
+public class PilaEstaticaRJC implements Pila<Object>{
+
+	@Override
+	public void introducir(Object elemento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object sacar() throws PilaVaciaExcepcion {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object cima() throws PilaVaciaExcepcion {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean estaVacia() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int longitud() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
