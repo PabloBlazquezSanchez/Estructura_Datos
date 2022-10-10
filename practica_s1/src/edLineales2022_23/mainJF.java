d75494b6c1893d01b6be6d34071ca233f3378853
Jesús <Jesus.Fdez1@alu.uclm.es>
2022-10-10 17:25:13 +0200

    j

--- practica_s1/src/edLineales2022_23/mainJF.java
+++ practica_s1/src/edLineales2022_23/mainJF.java
@@ -4,7 +4,7 @@ public class mainJF {
 
 	    public static void main (String[] args)
 	    {
-	        StackJF stack = new StaticStackJF(3);
+	        StackJF stack = new StaticStackJF(2);
 	 
 	        stack.push(1);      // inserting 1 in the stack
 	        stack.push(2);      // inserting 2 in the stack
@@ -13,7 +13,8 @@ public class mainJF {
 	        stack.pop();        // removing the top element (1)
 	 
 	        stack.push(3);      // inserting 3 in the stack
-	        stack.push(4);      // inserting 3 in the stack
+	        stack.push(4); 
+	        stack.push(5);// inserting 3 in the stack
 	 
 	        System.out.println("The top element is " + stack.top());
 	        System.out.println("The stack size is " + stack.size());
