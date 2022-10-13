df453af359f2c6d8ad5747c59732f41690886b69
RaulJDlCRUZ <114936538+RaulJDlCRUZ@users.noreply.github.com>
2022-10-13 11:56:57 +0200

    PILA TERMINADA. MISMO RESULTADO EN DYNAMIC.
    PABLO, ES TU TURNO DE HACER LOS EJEMPLOS

---
+++ practica_s1/src/edLineales2022_23/MainDyamic.java
@@ -0,0 +1,34 @@
+package edLineales2022_23;
+
+public class MainDyamic {
+	
+	public static void main(String [] args) {
+		
+		PilaDinamicaRJC<Integer> stack = new PilaDinamicaRJC<Integer>();
+		
+        stack.push(1);      // inserting 1 in the stack
+        stack.push(2);      // inserting 2 in the stack
+ 
+        stack.pop();        // removing the top element (2)
+        stack.pop();        // removing the top element (1)
+ 
+        stack.push(3);      // inserting 3 in the stack
+        stack.push(4); 
+        stack.push(5);// inserting 3 in the stack
+ 
+        System.out.println("El elemento top es " + stack.top());
+        System.out.println("La longitud de la pila es " + stack.size());
+ 
+        stack.pop();        // removing the top element (3)
+ 
+        // check if the stack is empty
+        if (stack.isEmpty()) {
+            System.out.println("La pila esta vacia");
+        }
+        else {
+            System.out.println("La pila no esta vacia");
+        }
+        System.out.println("El elemento top es " + stack.top());		
+		
+	}
+}
