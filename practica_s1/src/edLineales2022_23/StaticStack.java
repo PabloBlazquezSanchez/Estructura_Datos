c2d85bc3c8f7a277b105610e6dc28530135f584a
Jesús <Jesus.Fdez1@alu.uclm.es>
2022-10-16 20:37:05 +0200

    k

--- practica_s1/src/edLineales2022_23/StaticStack.java
+++ practica_s1/src/edLineales2022_23/StaticStack.java
@@ -1,53 +1,54 @@
 package edLineales2022_23;
 
-public class StaticStack<R extends Object> implements Pila<R>{
+public class StaticStack<R extends Object> implements Pila<R> {
 	private int longitud, cima;
 	private R[] S;
-	
-	
 
 	public StaticStack(int longitud) {
 		this.longitud = longitud;
 		this.cima = -1;
-		S = (R[]) new Object[longitud]; //usamos molde Object (limitacion java)
+		S = (R[]) new Object[longitud]; // usamos molde Object (limitacion java)
 	}
 
-	@Override
 	public void push(R element) {
-		if(size()==longitud) {
-			//throw new StackOverflowExceptionRJC("Stack Overflow.");
-			R[] nS = (R[]) new Object[this.longitud * 2];
-			for (int i = 0; i < longitud; i++) {nS[i] = this.S[i];}
-			this.S = nS;
+		if (size() == longitud) {
+			// throw new StackOverflowExceptionRJC("Stack Overflow.");
+			R[] newStack = (R[]) new Object[this.longitud * 2];
+			for (int i = 0; i < longitud; i++) {
+				newStack[i] = this.S[i];
+			}
+			this.S = newStack;
 			this.longitud *= 2;
 		}
-		S[++cima]=element;
+		S[++cima] = element;
 	}
 
-	@Override
+	
 	public R pop() throws EmptyStackExceptionRJC {
 		R element;
-		if(isEmpty()) throw new EmptyStackExceptionRJC("\nThe stack is empty.");
+		if (isEmpty())
+			throw new EmptyStackExceptionRJC("\n La pila está vacía.");
 		else {
-			element = S[cima];
-			cima--;
+			element = S[cima--];
 		}
 		return element;
 	}
 
-	@Override
+	
 	public R top() throws EmptyStackExceptionRJC {
-		if(isEmpty()) throw new EmptyStackExceptionRJC("\nThe stack is empty.");
-		else return S[cima];
+		if (isEmpty())
+			throw new EmptyStackExceptionRJC("\nThe stack is empty.");
+		else
+			return S[cima];
 	}
 
-	@Override
+	
 	public boolean isEmpty() {
 		return size() == 0;
 	}
 
-	@Override
+	
 	public int size() {
-		return cima+1;
+		return cima + 1;
 	}
 }
