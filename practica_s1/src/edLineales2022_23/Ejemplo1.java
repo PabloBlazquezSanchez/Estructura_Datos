7d35fce7d8c6d591bd40551b737d56833c8a9954
PabloBlazquezSanchez <Pablo.Blazquez1@alu.uclm.es>
2022-10-17 16:56:48 +0200

    Cambios

--- practica_s1/src/edLineales2022_23/Ejemplo1.java
+++ practica_s1/src/edLineales2022_23/Ejemplo1.java
@@ -1,41 +1,39 @@
 package edLineales2022_23;
 
-import java.util.Iterator;
-
 public class Ejemplo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StaticStack<Integer> pila_e = new StaticStack<Integer>(6);
-		PilaDinamicaRJC<Integer> pila_d = new PilaDinamicaRJC<Integer>();
+		Stack<Integer> pila = new StaticStack<Integer>(6);
 		
 		System.out.println("Pila Estática: ");
-		pila_e.push(3);
-		pila_e.push(67);
-		pila_e.push(45);
-		pila_e.push(9);
-		pila_e.push(28);
-		pila_e.push(33);
+		pila.push(3);
+		pila.push(67);
+		pila.push(45);
+		pila.push(9);
+		pila.push(28);
+		pila.push(33);
 		
-		for(int i = pila_e.size(); i >= 0; i--) {
-			System.out.print(pila_e.pop() + " ");
-			if(pila_e.isEmpty()) {
+		for(int i = pila.size(); i >= 0; i--) {
+			System.out.print(pila.pop() + " ");
+			if(pila.isEmpty()) {
 				break;
 			}
 		}
 		
+		pila = new PilaDinamicaRJC<Integer>();
 		System.out.println("\nPila Dinámica: ");
 		
-		pila_d.push(3);
-		pila_d.push(67);
-		pila_d.push(45);
-		pila_d.push(9);
-		pila_d.push(28);
-		pila_d.push(33);
+		pila.push(3);
+		pila.push(67);
+		pila.push(45);
+		pila.push(9);
+		pila.push(28);
+		pila.push(33);
 		
-		for(int i = pila_d.size(); i >= 0; i--) {
-			System.out.print(pila_d.pop() + " ");
-			if(pila_d.isEmpty()) {
+		for(int i = pila.size(); i >= 0; i--) {
+			System.out.print(pila.pop() + " ");
+			if(pila.isEmpty()) {
 				break;
 			}
 		}
