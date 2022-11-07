669b436c6b5986bdae992a3300f17e402fa1031e
Jesús <Jesus.Fdez1@alu.uclm.es>
2022-11-07 17:38:46 +0100

    w

---
+++ EDA_PRACTICA3/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edLineales2022_23","l":"Termino"}];updateSearchResults();
--- EDA_PRACTICA3/src/edLineales2022_23/Termino.java
+++ EDA_PRACTICA3/src/edLineales2022_23/Termino.java
@@ -1,23 +1,60 @@
 package edLineales2022_23;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Termino.
+ */
 public class Termino {
+
+/** The definicion. */
 private String definicion;
+
+/** The nombre. */
 private String nombre;
 
+/**
+ * Instantiates a new termino.
+ *
+ * @param nombre the nombre
+ * @param definicion the definicion
+ */
 public Termino(String nombre, String definicion) {
 	this.definicion = definicion;
 	this.nombre = nombre;
 }
 
+/**
+ * Gets the definicion.
+ *
+ * @return the definicion
+ */
 public String getDefinicion() {
 	return definicion;
 }
+
+/**
+ * Sets the definicion.
+ *
+ * @param definicion the new definicion
+ */
 public void setDefinicion(String definicion) {
 	this.definicion = definicion;
 }
+
+/**
+ * Gets the nombre.
+ *
+ * @return the nombre
+ */
 public String getNombre() {
 	return nombre;
 }
+
+/**
+ * Sets the nombre.
+ *
+ * @param nombre the new nombre
+ */
 public void setNombre(String nombre) {
 	this.nombre = nombre;
 }
