d357e7c9bc31fbd5272df59e81163ddb89b5b6d3
Jesús <Jesus.Fdez1@alu.uclm.es>
2022-12-12 00:08:18 +0100

    t

--- EDA_PRACTICA_NL1/src/edNoLineales2022_23/Heroe.java
+++ EDA_PRACTICA_NL1/src/edNoLineales2022_23/Heroe.java
@@ -2,15 +2,38 @@ package edNoLineales2022_23;
 
 import Grafo.Element;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Heroe.
+ *
+ * @param <E> the element type
+ */
 public class Heroe<E> implements Element {
 
+	/** The nombre heroe. */
 	private String nombreHeroe;
+	
+	/** The num interacciones. */
 	private int numInteracciones;
+	
+	/** The visitado. */
 	private boolean visitado;
+	
+	/** The parent. */
 	private Heroe<E> parent;
+	
+	/** The distancia. */
 	private int distancia;
+	
+	/** The element. */
 	private E element;
 
+	/**
+	 * Instantiates a new heroe.
+	 *
+	 * @param nombreHeroe the nombre heroe
+	 * @param element the element
+	 */
 	public Heroe(String nombreHeroe, E element) {
 		this.element = element;
 		this.nombreHeroe = nombreHeroe;
@@ -19,54 +42,102 @@ public class Heroe<E> implements Element {
 		distancia = 0;
 	}
 
-	
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
 	public String getID() {
 		return nombreHeroe.toString();
 	}
 
+	/**
+	 * Sets the nombre.
+	 *
+	 * @param nombre the new nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombreHeroe = nombre;
 	}
 
-	
+	/**
+	 * Sets the visited.
+	 *
+	 * @param visited the new visited
+	 */
 	public void setVisited(boolean visited) {
 		this.visitado = visitado;
 	}
 
-
+	/**
+	 * Gets the visited.
+	 *
+	 * @return the visited
+	 */
 	public boolean getVisited() {
 		return visitado;
 	}
 
-
+	/**
+	 * Gets the parent.
+	 *
+	 * @return the parent
+	 */
 	public Heroe<E> getParent() {
 		return parent;
 	}
 
-
+	/**
+	 * Sets the parent.
+	 *
+	 * @param parent the new parent
+	 */
 	public void setParent(Heroe<E> parent) {
 		this.parent = parent;
 	}
 
-
+	/**
+	 * Gets the element.
+	 *
+	 * @return the element
+	 */
 	public E getElement() {
 		return element;
 	}
 
+	/**
+	 * Gets the distance.
+	 *
+	 * @return the distance
+	 */
 	public int getDistance() {
 		return distancia;
 	}
 
+	/**
+	 * Sets the distance.
+	 *
+	 * @param d the new distance
+	 */
 	public void setDistance(int d) {
 		distancia = d;
 	}
 
-
+	/**
+	 * Equals.
+	 *
+	 * @param n the n
+	 * @return true, if successful
+	 */
 	public boolean equals(Heroe n) {
 		return ((nombreHeroe.equals(n.getID())) && (element.equals(n.getElement())));
 	}
 
-
+	/**
+	 * To string.
+	 *
+	 * @return the string
+	 */
 	public String toString() {
 		return element.toString();
 	}
--- EDA_PRACTICA_NL1/src/edNoLineales2022_23/Relacion.java
+++ EDA_PRACTICA_NL1/src/edNoLineales2022_23/Relacion.java
@@ -2,19 +2,45 @@ package edNoLineales2022_23;
 
 import Grafo.Element;
 
-public class Relacion <E> implements Element{
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Relacion.
+ *
+ * @param <E> the element type
+ */
+public class Relacion<E> implements Element {
+
+	/** The n lineas. */
 	private Integer nLineas;
+
+	/** The peso. */
 	private int peso;
 
+	/**
+	 * Instantiates a new relacion.
+	 *
+	 * @param n the n
+	 * @param p the p
+	 */
 	public Relacion(int n, int p) {
 		nLineas = n;
 		peso = p;
 	}
 
+	/**
+	 * Gets the peso.
+	 *
+	 * @return the peso
+	 */
 	public int getPeso() {
 		return peso;
 	}
 
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
 	public String getID() {
 		return nLineas.toString();
 	}
