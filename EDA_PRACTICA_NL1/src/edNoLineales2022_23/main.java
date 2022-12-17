e11e5d3312df83b160bf106353bf864cf2d975d0
Jesús <Jesus.Fdez1@alu.uclm.es>
2022-12-17 01:10:02 +0100

    w

--- EDA_PRACTICA_NL1/src/edNoLineales2022_23/main.java
+++ EDA_PRACTICA_NL1/src/edNoLineales2022_23/main.java
@@ -317,7 +317,6 @@ public class main {
 
 		if (notermina)
 			w2.getElement().setParent(null);
-		// return w2.getElement();
 		return s;
 	}
 
@@ -346,7 +345,6 @@ public class main {
 			if (!w1.getElement().getVisited()) {
 				(w1.getElement()).setVisited(true);
 				st.push(w1.getElement());
-				System.out.println(w1.getElement().getnombreHeroe());
 					noEnd = algoritmoDFS(gr, w1, v2, st);
 					if (noEnd) {st.pop();}
 
